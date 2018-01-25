5a89a4525abc332cc450dfe862522893c5476f3e
Harley Rayner <harleymteden@gmail.com>
2018-01-25 06:44:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@
 #   elsif array.size == 2
 #   array.join(" and ")
 # elsif array.size == 3
-#   array = (array[0].to_s + array[1].to_s + "and " + array[2].to_s)
+#   array = (array[0].join + array[1].join + "and " + array[2].join)
 #   array.join(", ")
 #   binding.pry
 #   end
