cda5a22511fa2fdab325bc257ed9b1f43575d898
Harley Rayner <harleymteden@gmail.com>
2018-01-25 06:46:09 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -20,7 +20,7 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   elsif array.size == 3
-    array = [array[0], array[1], "and ", array[2]
+    array = [array[0], array[1], "and ", array[2]]
     array.join(", ")
   binding.pry
   end
