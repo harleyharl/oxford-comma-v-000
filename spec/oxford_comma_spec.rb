87c3d88cdbe2412e30ddb91b088551dfb1604639
Harley Rayner <harleymteden@gmail.com>
2018-01-25 06:26:44 +0000

    automatically backed up by learn

--- spec/oxford_comma_spec.rb
+++ spec/oxford_comma_spec.rb
@@ -1,4 +1,4 @@
-require 'Pry'
+require 'pry'
 describe "#oxford_comma" do
   it 'returns a string without any additional fomatting when given a 1-element array' do
     expect(oxford_comma(["kiwi"])).to eq("kiwi")
