05c16365758fcac68c7a18d743b799896bd81dac
Harley Rayner <harleymteden@gmail.com>
2018-01-25 06:39:51 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
+# def oxford_comma(array)
+#   if array.size == 1
+#   array = array.join
+#   elsif array.size == 2
+#   array.join(" and ")
+# elsif array.size == 3
+#   array = (array[0].to_s + array[1].to_s + "and " + array[2].to_s)
+#   array.join(", ")
+#   binding.pry
+#   end
+# end
+
+
+# = "and #{array[2].join}".to_s
+
+
 def oxford_comma(array)
   if array.size == 1
   array = array.join
@@ -11,6 +27,3 @@ elsif array.size == 3
   # binding.pry
   end
 end
-
-
-# = "and #{array[2].join}".to_s
