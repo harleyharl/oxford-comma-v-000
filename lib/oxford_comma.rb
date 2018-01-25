1970d917da2463599c88e0919aed4b10b6590718
Harley Rayner <harleymteden@gmail.com>
2018-01-25 06:21:51 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2
   array.join(" and ")
 elsif array.size == 3
-  array[2] == "and #{array[2]}"
+  array[2] == "and #{array[2]}".to_s
   array = array[0..2].join(" , ")
   end
 end
